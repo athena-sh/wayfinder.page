23a86cdd39036ceb4c6e48363d3c074ff939d68e
anitguru <me@anit.guru>
2025-05-04 23:24:58 -0400

    C

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -115,7 +115,7 @@ const config: Config = {
             },
             {
               label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
+              href: "https://github.com/athena-sh/wayfinder.page",
             },
           ],
         },
